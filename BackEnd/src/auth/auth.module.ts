70ed3082c594c1037d197cad13e9d4cb64c7a2f9
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-02-02 21:45:59 -0300

    update expiresIn

--- BackEnd/src/auth/auth.module.ts
+++ BackEnd/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '600s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
